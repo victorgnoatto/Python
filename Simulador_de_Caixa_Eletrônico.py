700cc3d96b332e0ac3a3dc1c76a34cb05ba2b946
Victor Gnoatto Batista <74217980+victorgnoatto@users.noreply.github.com>
2021-07-29 12:04:02 -0300

    Add files via upload

---
+++ Simulador_de_Caixa_Eletrônico.py
@@ -0,0 +1,25 @@
+print('=' * 30)
+print('{:^30}'.format('BANCO V&G'))
+print('=' * 30)
+valor = int(input('Que valor você quer sacar? R$'))
+total = valor
+ced = 50
+totced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totced += 1
+    else:
+        if totced > 0:
+            print(f'Total de {totced} cédulas de R${ced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totced = 0
+        if total == 0:
+            break
+print('=' * 30)
+print('Volte sempre ao BANCO V&G! Tenha um bom dia !')
